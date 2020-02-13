3e8a1fc938e88d076fa2bb01796cfc5efbd885e8
niju7957 <niju7957@colorado.edu>
2020-02-13 14:36:11 -0700

    create board function not quite working yet

---
+++ TicTacToe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int createBoard(){
+    int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
+    // for(int i = 0; i < 3; i++){
+    //     for(int j = 0; j < 3; j++){
+    //         arr[i][j] = 7;
+    //     }
+    // }
+    return arr;
+}
