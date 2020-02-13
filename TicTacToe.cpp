7de3a733b49a22c20b24e7cd204b9e63b71ee781
niju7957 <niju7957@colorado.edu>
2020-02-13 16:07:11 -0700

    Final verfsion of tic tac toe game and main are completed

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -18,35 +18,25 @@ void *createBoard(){
 }
 
 
-void DisplayBoard(int B[][3]) {
+void DisplayBoard(int B[3][3]) {
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			std::cout << (j < 3 ? std::to_string(B[i][j]) + "\t" : std::to_string(B[i][j]));
+			// std::cout << (j < 3 ? std::to_string(B[i][j]) + "\t" : std::to_string(B[i][j]));
+            cout << " " << B[i][j] <<" ";
 		}
 		std::cout << "\n";
 	}
 }
 
-void PlaceMarker(Position p, int marker, int B[][3]) {
+void PlaceMarker(Position p, int marker, int B[3][3]) {
 
 	B[p.row][p.col] = marker;
 
 }
-      
-int main(){
-    int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
-
-    bool winner = winner(arr);
 
 
-
-
-    return 0;
-}
-
-
-bool winner(int arr[3][3]){
+bool Winner(int arr[3][3]){
     if(arr[0][0] == arr[0][1] == arr[0][2]){
 
         return true;
@@ -78,16 +68,48 @@ bool winner(int arr[3][3]){
 }
 
 Position GetPlayerChoice(){
-Position desierd_position = new Position;
+Position desierd_position;
 int pos_row, pos_col;
 cout<<"Which row do you want to put your pieces."<<endl;
 cin>> pos_row;
-cout<<"Which column do you want to put your pieces."<<endl
+cout<<"Which column do you want to put your pieces."<<endl;
 cin>> pos_col;
 
 desierd_position.row = pos_row;
 desierd_position.col = pos_col;
 
 
-return desierd_position
+return desierd_position;
+}
+
+
+
+int main(){
+    int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
+
+
+    bool win = false;
+    int player = 1;
+    Position pos;
+
+    while(!win){
+        pos = GetPlayerChoice();
+        PlaceMarker(pos, player, arr);
+        win = Winner(arr);
+        if(!win){
+            if(player == 1){
+                player = 2;
+            }
+            else{
+                player = 1;
+            }
+        }
+        DisplayBoard(arr);
+    }
+    cout << "Congrats! Player " << player << " won!" << endl;
+
+
+
+
+    return 0;
 }
