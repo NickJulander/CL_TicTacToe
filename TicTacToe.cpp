ed7b03dede771a67d043c27a3e1d1678fffdab84
njnxes <najo6550@colorado.edu>
2020-02-13 15:12:03 -0700

    person2 changes

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -8,6 +8,16 @@ void *createBoard(){
     return *arr;
 }
 
+void DisplayBoard(int B[][3]) {
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			std::cout << (j < 3 ? std::to_string(B[i][j]) + "\t" : std::to_string(B[i][j]));
+		}
+		std::cout << "\n";
+	}
+}
+
 int main(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
     return 0;
