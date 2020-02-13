8f82be7aa44771359a0472d7ecd54c01313ff92e
Nick <nija3172@colorado.edu>
2020-02-13 15:05:49 -0700

    Adding DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,7 +7,14 @@ void *createBoard(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
     return *arr;
 }
-
+void DisplayBoard(int arr[3][3]){
+    for(int i = 0; i < 3; i ++){
+      for(int j = 0; j < 3; j ++){
+          std::cout<<arr[i][j];
+      }
+      std::cout<<'/n';
+    }
+}
 int main(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
     return 0;
