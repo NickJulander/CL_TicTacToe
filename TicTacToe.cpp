470fa5a27d06f0a1a49639b804828926fc1016a6
niju7957 <niju7957@colorado.edu>
2020-02-13 14:53:45 -0700

    Created a create board function and a main that inits our board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
-int createBoard(){
+void *createBoard(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
-    // for(int i = 0; i < 3; i++){
-    //     for(int j = 0; j < 3; j++){
-    //         arr[i][j] = 7;
-    //     }
-    // }
-    return arr;
+    return *arr;
+}
+
+int main(){
+    int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
+    return 0;
 }
