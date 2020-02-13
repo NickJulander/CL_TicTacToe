3b65a9896549ba1fc435e9439294363a3841c796
niju7957 <niju7957@colorado.edu>
2020-02-13 15:34:53 -0700

    started working on the code to run tic tac toe, need some functions to be complete though

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,6 +7,8 @@ void *createBoard(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
     return *arr;
 }
+
+
 void DisplayBoard(int arr[3][3]){
     for(int i = 0; i < 3; i ++){
       for(int j = 0; j < 3; j ++){
@@ -15,7 +17,46 @@ void DisplayBoard(int arr[3][3]){
       std::cout<<'/n';
     }
 }
+
+
 int main(){
     int arr[3][3] = {{7, 7, 7}, {7, 7, 7}, {7, 7, 7}};
+
+    bool winner = winner(arr);
+
+
+
+
     return 0;
+}
+
+bool winner(int arr[3][3]){
+    if(arr[0][0] == arr[0][1] == arr[0][2]){
+
+        return true;
+    }
+    if(arr[1][0] == arr[1][1] == arr[1][2]){
+        return true;
+    }
+    if(arr[2][0] == arr[2][1] == arr[2][2]){
+        return true;
+    }
+    if(arr[0][0] == arr[1][0] == arr[2][0]){
+        return true;
+    }
+    if(arr[0][1] == arr[1][1] == arr[2][1]){
+        return true;
+    }
+    if(arr[0][2] == arr[1][2] == arr[2][2]){
+        return true;
+    }
+    if(arr[0][0] == arr[1][1] == arr[2][2]){
+        return true;
+    }
+    if(arr[0][2] == arr[1][1] == arr[2][0]){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
